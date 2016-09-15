8ba4de0578f0e6f64a558db83668fec029da62ad
Gumio <quran.gumi3134@gmail.com>
2016-09-16 08:52:24 +0900

    pumaの設定

--- config/puma.rb
+++ config/puma.rb
@@ -37,8 +37,6 @@ on_worker_boot do
   # deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
 end
-# workers ENV.fetch("WEB_CONCURRENCY") { 2 }
-
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
 # before forking the application. This takes advantage of Copy On Write
