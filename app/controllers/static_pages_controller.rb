8a70b1678a6ce6559e4cc6422be4336924da6c8a
Gumio <quran.gumi3134@gmail.com>
2016-09-20 12:28:06 +0900

    homeアクションにフィードのインスタンス変数を追加

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if logged_in?
+    if logged_in?
+      @micropost = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: prarams[:page])
   end
 
   def help
